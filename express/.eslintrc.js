058cad3a2534cccf775266e1d1a7f7a90b838199
Taylor Abraham <taylor.abra8@gmail.com>
2021-12-18 00:44:27 -0500

    Add typescript

--- express/.eslintrc.js
+++ express/.eslintrc.js
@@ -2,8 +2,9 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true
   },
-  extends: ['plugin:react/recommended', 'airbnb'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -11,35 +12,16 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier'],
+  plugins: ['prettier'],
   rules: {
-    // Only ok in server files
-    'no-console': 0,
-    // Allow braces around return-only arrow functions, for better readability
-    'arrow-body-style': 0,
-    // Allow arrow functions without return statements
-    'consistent-return': 0,
-    // Allow the convention where the non-connected component is exported, and the
-    // component using connect is exported as default. This is made to allow mocking Redux.
-    'import/no-named-as-default': 0,
-    'no-nested-ternary': 0,
-    'no-plusplus': 0,
     // Allow unused variables if they are prefixed with an underscore
-    'no-unused-vars': [
+    '@typescript-eslint/no-unused-vars': [
       'warn',
       {
         argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
       },
     ],
-    // Allow the use of non-template strings when it would be cleaner to do so
-    'prefer-template': 0,
-    // Disable prop types. Instead, create TypeScript prop interfaces
-    'react/prop-types': 0,
-    // This rule conflicts with Prettier
-    'react/jsx-curly-newline': 0,
-    // RN has trouble with jsx files, so we can't use them
-    'react/jsx-filename-extension': 0,
-    // Allow prop spreading for custom components
-    'react/jsx-props-no-spreading': 0,
   },
 };
